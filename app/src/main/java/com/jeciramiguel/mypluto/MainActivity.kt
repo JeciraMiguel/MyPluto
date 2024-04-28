9a27f8dece22b13f3ab5ef453eee5ffe3521c98b
JeciraMiguel <166519131+JeciraMiguel@users.noreply.github.com>
2024-04-28 17:23:41 +0200

    Welcome screen logic
    I used the Intent package to change the screen when I click on the "get started" button

--- app/src/main/java/com/jeciramiguel/mypluto/MainActivity.kt
+++ app/src/main/java/com/jeciramiguel/mypluto/MainActivity.kt
@@ -9,5 +9,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Reference the start button from the layout
+        val startButton: Button = findViewById<Button>(R.id.startButton)
+
+        // Set a click listener on the start button
+        startButton.setOnClickListener {
+            // Create an Intent to start the GameActivity
+            val intent = Intent(this, GameActivity::class.java)
+            // Start the GameActivity
+            startActivity(intent)
+        }
     }
 }
